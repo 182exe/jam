31627340f2d29ff8097d28bb3597ae8aa1bf3986
182exe <81325987+182exe@users.noreply.github.com>
2023-05-16 12:30:27 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,23 @@
-var CACHE_NAME = "cache-v1",
-    urlsToCache = ["/", "/dontleave.html"];
-self.addEventListener("install", function(e) {
-    e.waitUntil(caches.open(CACHE_NAME).then(function(e) {
-        return console.log("Opened cache"), e.addAll(urlsToCache)
+var cacheid = "cache-v1",
+    cachedUrls = ["/", "/dontleave.html"];
+self.addEventListener("install", function (e) {
+    e.waitUntil(caches.open(cacheid).then(function (e) {
+        return console.log("Opened cache"), e.addAll(cachedUrls)
     }))
-}), self.addEventListener("fetch", function(e) {
-    e.respondWith(caches.match(e.request).then(function(n) {
-        return n || fetch(e.request).then(function(n) {
+}), self.addEventListener("fetch", function (e) {
+    e.respondWith(caches.match(e.request).then(function (n) {
+        return n || fetch(e.request).then(function (n) {
             if (!n || 200 !== n.status || "basic" !== n.type) return n;
             var t = n.clone();
-            return caches.open(CACHE_NAME).then(function(n) {
+            return caches.open(cacheid).then(function (n) {
                 n.put(e.request, t)
             }), n
         })
     }))
-}), self.addEventListener("activate", function(e) {
+}), self.addEventListener("activate", function (e) {
     var n = ["cache-v7"];
-    e.waitUntil(caches.keys().then(function(e) {
-        return Promise.all(e.map(function(e) {
+    e.waitUntil(caches.keys().then(function (e) {
+        return Promise.all(e.map(function (e) {
             if (-1 === n.indexOf(e)) return caches.delete(e)
         }))
     }))
